9d109db28294661f2ad0b966ec464d6b3a00602a
Zack Honey <zhoney466@gmail.com>
2023-10-08 12:30:56 -0700

    can't get anything to retrieve from database

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -4,17 +4,33 @@ const Action = require('./actions-model')
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
+router.get('/', (req, res, next) => {
     Action.get(req.query)
-    .then(actions => {
-        res.status(200).json(actions || [])
-    })
-    .catch(next)
+        .then(actions => {
+            res.status(200).json(actions || [])
+        })
+        .catch(next)
 });
 
+router.get('/:id', (req, res, next) => {
+    Action.get(req.params.id)
+        .then(action => {
+            if (action) {
+                res.status(200).json(action)
+            } else {
+                res.status(404).json({
+                    message: 'could not retrieve action'
+                })
+            }
+        })
+        .catch(next)
+})
 
-router.use((err, req, res, next ) => {// eslint-disable-line
-    res.status(err.status || 500 ).json({
+
+
+
+router.use((err, req, res, next) => {// eslint-disable-line
+    res.status(err.status || 500).json({
         customMessage: 'something bad happened inside actions router',
         message: err.message,
         stack: err.stack
